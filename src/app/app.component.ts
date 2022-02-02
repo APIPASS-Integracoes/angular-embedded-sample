05589485e69a066a868dd883ae303497195c17d4
Raphael Santos Custódio <raphael5178c@gmail.com>
2022-02-02 14:55:03 -0300

    sdk support update npm version support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {DomSanitizer} from "@angular/platform-browser";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 import {HttpClient} from "@angular/common/http";
+import {ApipassSDK} from "@apipass/apipass-api-js-sdk/src/apipass-api-js-sdk";
 
 @Component({
   selector: 'app-root',
@@ -10,23 +11,30 @@ import {HttpClient} from "@angular/common/http";
 export class AppComponent implements OnInit {
   title = 'angular-embedded-sample';
   public accessToken = '<your-access-token>';
-  // @ts-ignore
-  public iframeUrl;
+  public iframeUrl: SafeResourceUrl | undefined;
+  public company: any;
 
   constructor(protected _sanitizer: DomSanitizer,
               private httpClient: HttpClient) {
   }
 
-  openClassroom() {
+  async openClassroom() {
     this.iframeUrl = this._sanitizer.bypassSecurityTrustResourceUrl(`https://authorization.apipass.com.br/authorization/new-authorization?token=${this.accessToken}&provider=GOOGLE_CLASSROOM`)
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const token = '<YOUR TOKEN>';
+    await ApipassSDK.init(token);
+    this.company = await ApipassSDK.findCompany();
+    this.watchAuthorizationSaved();
+  }
+
+  private watchAuthorizationSaved() {
     // After the authorization is saved, the iframe emits an message event with the saved authorization data
     window.addEventListener('message', ({data}) => {
       if (data?.savedAuthorization) {
         // Here we get the saved authorization id and send it to an APIPASS flow, where we create a link of the authorization id with your costumer identifier
-        this.httpClient.post('https://core.apipass.com.br/api/<your-apipass-company-id>/activate-auth', {
+        this.httpClient.post(`https://core.apipass.com.br/api/${this.company?.id}/activate-auth`, {
           authorizationId: data.savedAuthorization.id,
           customerId: '<your-customer-id>'
         }).subscribe(value => {
@@ -36,4 +44,5 @@ export class AppComponent implements OnInit {
       }
     });
   }
+
 }
